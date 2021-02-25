60261751bcaab2657d3b71c56b53160fdf2a1b2b
alco99 <76519958+alco99@users.noreply.github.com>
2021-02-25 20:49:39 +0800

    Create turtle_drawing_heart.py

---
+++ turtle_drawing_heart.py
@@ -0,0 +1,29 @@
+import turtle
+tina=turtle.Turtle()
+tina.shape('turtle')
+
+import math
+k=40
+h=math.sqrt((k**2)+(k**2))
+l=math.sqrt((140**2)+(100**2))
+
+tina.left(135)
+tina.forward(h)
+tina.left(45)
+tina.forward(k)
+tina.left(45)
+tina.forward(h)
+tina.left(45)
+tina.forward(k)
+tina.left(45)
+tina.forward(l)
+tina.left(90)
+tina.forward(l)
+tina.left(45)
+tina.forward(k)
+tina.left(45)
+tina.forward(h)
+tina.left(45)
+tina.forward(k)
+tina.left(45)
+tina.forward(h)
